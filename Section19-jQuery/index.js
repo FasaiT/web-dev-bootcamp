bd54a9b05fd873ce07da148447227432c16f470d
fasai1470 <fasai1470@gmail.com>
2024-09-04 18:26:54 +0700

    Fix: Add folder Section19-jQuery and code

---
+++ Section19-jQuery/index.js
@@ -0,0 +1,16 @@
+
+    // $("h1").css("color", "green");
+
+    $("h1").text("Bye");
+
+    $("h1").addClass("big-title margin-50");
+    
+    if($("h1").hasClass("margin-50") == true) {
+        console.log("HAS CLASS 'margin-50'");
+    }
+
+    $("button").html("<em>Hey</em>");
+
+    $("img").attr("src");
+    
+    $("a").attr("href", "https://www.yahoo.com");
