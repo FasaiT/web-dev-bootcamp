bedf97b894e01dce37aa12edb649a3e7fdfc4d0b
fasai1470 <fasai1470@gmail.com>
2024-09-08 21:57:08 +0700

    Fix: Add code answer4

--- Section20-Boss-Level-Challenge-2-The-Simon-Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Section20-Boss-Level-Challenge-2-The-Simon-Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -7,10 +7,11 @@ var randomChosenColour = buttonColours[randomNumber];
 // return randomChosenColour;
 gamePattern.push(randomChosenColour);
 $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-}
 
 var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
 audio.play();
+}
+
 
 console.log(nextSequence());
 console.log(gamePattern);
