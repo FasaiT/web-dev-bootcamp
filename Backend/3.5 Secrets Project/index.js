374e6fc9ae019063c65edf3c96635bec41b7bac5
fasai1470 <fasai1470@gmail.com>
2025-03-03 16:46:57 +0700

    Add: file 3.5 Secrets Project

---
+++ Backend/3.5 Secrets Project/index.js
@@ -0,0 +1,47 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+//1.รับค่าinputจากหน้าบ้านก่อน
+//2.สร้าง
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+    userIsAuthorised = false;
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
